5fefebbf929e2cd07872df1ac7758004946d6f55
ThePiranha <sabah_365@hotmail.com>
2017-01-04 08:14:10 -0500

    FIrst commit

---
+++ ComboBox_To_TextBox/Form1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ComboBox_To_TextBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = comboBox1.SelectedItem.ToString();
+        }
+    }
+}
